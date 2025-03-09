f720867dba026cffadbc0c19176c2437a00ded04
zlz <1852781063@qq.com>
2025-03-09 16:16:47 +0800

    feat(all): finish next.js project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'customer',
+}
 const Page = () => {
   return <div>customers page</div>
 }
--- app/dashboard/page.tsx
+++
@@ -1,4 +0,0 @@
-const Page = () => {
-  return <div>Dashboard page</div>
-}
-export default Page
